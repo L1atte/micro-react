7548ed200dcd4ae189fe9eb8d78c9a979bfddb90
L1atte <l4atte@gmail.com>
2023-01-31 02:15:16 +0800

    feat: Concurrent Mode

--- micro-react/render.js
+++ micro-react/render.js
@@ -7,12 +7,32 @@ export function render(element, container) {
 
 	// 赋值属性
 	Object.keys(element.props)
-		.filter((key) => key !== "children")
-		.forEach((key) => (dom[key] = element.props[key]));
-
-	// 递归，渲染每一个child
-	element.props.children.forEach((child) => render(child, dom));
+		.filter(key => key !== "children")
+		.forEach(key => (dom[key] = element.props[key]));
 
 	// 将子节点添加到父节点下
 	container.append(dom);
+}
+
+let nextUnitOfWork = null;
+
+// 调度函数
+function workLoop(deadline) {
+	// 是否退出
+	let shouldYield = false;
+
+	while (nextUnitOfWork && !shouldYield) {
+		nextUnitOfWork = performUnitOfWork(nextUnitOfWork);
+		shouldYield = deadline.timeRemaining() < 1;
+	}
+
+	// 没有剩余时间，退出循环。将工作交给 requestIdleCallback 调度
+	requestIdleCallback(workLoop);
+}
+
+// 第一次请求
+requestIdleCallback(workLoop);
+
+function performUnitOfWork(nextUnitOfWork) {
+	// TODO
 }
