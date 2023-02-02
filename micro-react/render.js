c425fc9a67e2b84e3db5050ce1579d6459d541d9
L1atte <l4atte@gmail.com>
2023-02-02 08:30:58 +0800

    feat: fiber

--- micro-react/render.js
+++ micro-react/render.js
@@ -1,17 +1,26 @@
-export function render(element, container) {
+export function createDom(fiber) {
 	// 创建父节点
 	const dom =
-		element.type == "TEXT_ELEMENT"
+		fiber.type == "TEXT_ELEMENT"
 			? document.createTextNode("")
-			: document.createElement(element.type);
+			: document.createElement(fiber.type);
 
 	// 赋值属性
-	Object.keys(element.props)
+	Object.keys(fiber.props)
 		.filter(key => key !== "children")
-		.forEach(key => (dom[key] = element.props[key]));
+		.forEach(key => (dom[key] = fiber.props[key]));
 
-	// 将子节点添加到父节点下
-	container.append(dom);
+	return dom;
+}
+
+function render(element, container) {
+	// set next unit of work
+	nextUnitOfWork = {
+		dom: container,
+		props: {
+			children: [element],
+		},
+	};
 }
 
 let nextUnitOfWork = null;
@@ -33,6 +42,43 @@ function workLoop(deadline) {
 // 第一次请求
 requestIdleCallback(workLoop);
 
-function performUnitOfWork(nextUnitOfWork) {
-	// TODO
+function performUnitOfWork(fiber) {
+	// add dom node
+	if (!fiber.dom) fiber.dom = createDom(fiber);
+
+	if (!fiber.parent) fiber.parent.dom.appendChild(fiber.dom);
+
+	// create new fibers
+	const elements = fiber.props.children;
+	let prevSibling = null;
+
+	for (let i = 0; i < elements.length; i++) {
+		const element = elements[index];
+
+		const newFiber = {
+			type: element.type,
+			props: element.props,
+			parent: fiber,
+			dom: null,
+		};
+
+		if (index === 0) {
+			fiber.child = newFiber;
+		} else {
+			prevSibling.sibling = newFiber;
+		}
+
+		prevSibling = newFiber;
+	}
+
+	if (fiber.child) {
+		return fiber.child;
+	}
+	let nextFiber = fiber;
+	while (nextFiber) {
+		if (nextFiber.sibling) {
+			return nextFiber.sibling;
+		}
+		nextFiber = nextFiber.parent;
+	}
 }
